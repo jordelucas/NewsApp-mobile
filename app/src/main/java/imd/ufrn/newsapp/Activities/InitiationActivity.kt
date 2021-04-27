a713d84d504f586a83fd3a2884184626e1d47439
Jordevá <jordevalucas@gmail.com>
2021-04-27 01:25:42 -0300

    adiciona layout da tela inicial

--- app/src/main/java/imd/ufrn/newsapp/Activities/InitiationActivity.kt
+++ app/src/main/java/imd/ufrn/newsapp/Activities/InitiationActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import imd.ufrn.newsapp.R
 
-class ConviteSignUpActivity : AppCompatActivity() {
+class InitiationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_convite_sign_up)
+        setContentView(R.layout.activity_initiation)
     }
 }
