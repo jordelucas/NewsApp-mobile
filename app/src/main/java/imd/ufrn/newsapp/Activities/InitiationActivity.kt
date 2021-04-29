1e0929db9391d22c667f302c2fa88998739289db
Marcelo Ferreira <marcelosekundo@gmail.com>
2021-04-28 23:14:12 -0300

    InitiationActivity como tela inicial e função dos botões

--- app/src/main/java/imd/ufrn/newsapp/Activities/InitiationActivity.kt
+++ app/src/main/java/imd/ufrn/newsapp/Activities/InitiationActivity.kt
@@ -4,28 +4,37 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import imd.ufrn.newsapp.R
 
 class InitiationActivity : AppCompatActivity() {
 
-    private lateinit var optLogin: TextView
-    private lateinit var optSignup: TextView
+    private lateinit var layLogin: LinearLayout
+    private lateinit var laySignUp: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initiation)
 
-        // optLogin = findViewById(R.id...)
-        // optSignup = findViewById(R.id...)
+        layLogin = findViewById(R.id.layLogin)
+        laySignUp = findViewById(R.id.laySignUp)
+
+        layLogin.setOnClickListener {
+            toLogin()
+        }
+
+        laySignUp.setOnClickListener {
+            toSignup()
+        }
     }
 
-    fun toLogin(view: View) {
+    fun toLogin() {
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
     }
 
-    fun toSignup(view: View) {
+    fun toSignup() {
         val intent = Intent(this, SignUpActivity::class.java)
         startActivity(intent)
     }
