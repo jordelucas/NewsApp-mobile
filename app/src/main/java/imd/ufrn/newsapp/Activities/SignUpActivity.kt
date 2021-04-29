1b2cc08e552310a1be13c8e96851dbde712e3185
Jordevá <jordevalucas@gmail.com>
2021-04-29 19:28:30 -0300

    Cadastro funcional

--- app/src/main/java/imd/ufrn/newsapp/Activities/SignUpActivity.kt
+++ app/src/main/java/imd/ufrn/newsapp/Activities/SignUpActivity.kt
@@ -1,12 +1,154 @@
 package imd.ufrn.newsapp.Activities
 
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import imd.ufrn.newsapp.R
+import org.json.JSONObject
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var edTxtNome: TextView
+    private lateinit var edTxtEmail: TextView
+    private lateinit var edTxtSenha: TextView
+    private lateinit var btnVoltar: Button
+    private lateinit var btnLogin: ImageButton
+
+    //TODO
+    private val TAG = "signUpTag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        edTxtNome = findViewById(R.id.edTxtSignUpNome)
+        edTxtEmail = findViewById(R.id.edTxtSignUpEmail)
+        edTxtSenha = findViewById(R.id.edTxtSignUpSenha)
+        btnVoltar = findViewById(R.id.btnSignUpVoltar)
+        btnLogin = findViewById(R.id.btnSignUpProximo)
+
+        btnVoltar.setOnClickListener {
+            toInitiation()
+        }
+
+        btnLogin.setOnClickListener {
+            register()
+        }
+    }
+
+    fun toInitiation() {
+        val intent = Intent(this, InitiationActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun register() {
+        var task = HTTPAuthenticationTask(
+            this,
+            "http://10.0.2.2:3333/users",
+            edTxtNome.text.toString(),
+            edTxtEmail.text.toString(),
+            edTxtSenha.text.toString()
+        )
+        task.execute()
+    }
+
+    class HTTPAuthenticationTask(
+        private var context: Context,
+        private var urlAddress: String,
+        private var nome: String,
+        private var email: String,
+        private var password: String,
+    ): AsyncTask<Int, Int, Unit>() {
+
+        private lateinit var pd: ProgressDialog
+        private var response =  String()
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            pd = ProgressDialog(context)
+            pd.setTitle("Cadastro")
+            pd.setMessage("Aguarde...")
+            pd.show()
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
+
+            pd.dismiss()
+
+            Log.i("tt", response)
+            val responseData = JSONObject(response)
+
+            val id = responseData.getString("id")
+            val name = responseData.getString("name")
+
+            val intent = Intent(context, MainActivity::class.java)
+            intent.putExtra("id", id)
+            intent.putExtra("name", name)
+
+            context.startActivity(intent)
+        }
+
+        override fun doInBackground(vararg params: Int?) {
+            var urlConnection: HttpURLConnection? = null
+
+            try {
+                val postData = JSONObject()
+                postData.put("name", nome)
+                postData.put("email", email)
+                postData.put("password", password)
+                postData.put("permission", "subscriber")
+
+                val url = URL(urlAddress)
+                urlConnection = url.openConnection() as HttpURLConnection
+                urlConnection.setRequestProperty("Content-Type", "application/json")
+                urlConnection.requestMethod = "POST"
+                urlConnection.doOutput = true
+                urlConnection.doInput = true
+
+                Log.i("tt", urlAddress)
+                Log.i("tt", postData.toString())
+
+                val out: OutputStream = BufferedOutputStream(urlConnection.outputStream)
+                val writer = BufferedWriter(OutputStreamWriter(out, "UTF-8"))
+                writer.write(postData.toString())
+                writer.flush()
+
+                val code = urlConnection.responseCode
+                if (code != 201) {
+                    throw IOException("Server response $code")
+                }
+
+                val rd = BufferedReader(
+                    InputStreamReader(
+                        urlConnection.inputStream
+                    )
+                )
+
+                val responseString = StringBuffer()
+                var line: String?
+                while (rd.readLine().also { line = it } != null) {
+                    responseString.append(line + "\n")
+                }
+                response = responseString.toString()
+                Log.i("tt", response)
+            }
+            catch (e: Exception) {
+                e.printStackTrace();
+            } finally {
+                urlConnection?.disconnect()
+            }
+        }
     }
 }
