74abc53786fe27bfe17a8d8b4ebc3b0e1c678ba3
Marcelo Ferreira <marcelosekundo@gmail.com>
2021-04-28 22:25:26 -0300

    Algumas alterações

--- app/src/main/java/imd/ufrn/newsapp/Activities/InitiationActivity.kt
+++ app/src/main/java/imd/ufrn/newsapp/Activities/InitiationActivity.kt
@@ -2,11 +2,24 @@ package imd.ufrn.newsapp.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import imd.ufrn.newsapp.R
 
 class InitiationActivity : AppCompatActivity() {
+
+    private lateinit var optLogin: TextView
+    private lateinit var optSignup: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initiation)
+
+        // optLogin = findViewById(R.id...)
+        // optSignup = findViewById(R.id...)
     }
+
+    fun toLogin(view: View) {}
+
+    fun toSignup(view: View) {}
 }
